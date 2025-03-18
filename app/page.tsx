aaf6e5c7e3a70ea8c6a9dcfc5c393e4da579bdab
Ahdwka <ahed.wka4g@gmail.com>
2025-03-19 02:08:55 +0300

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
   return (
     <div className="container text-center text-primary mx-auto Alexandria">
       <div className="text-center py-16">
-        <div className="lg:text-7xl md:text-6xl text- text-5xl leading-18 my-6">رازيو لأجهزة الكمبيوتر المحمولة</div>
-        <div className="lg:text-2xl text-lg opacity-75 mt-8">
+        <div className="lg:text-7xl md:text-6xl text-4xl md:leading-18 leading-14 md:my-6 my-4">رازيو لأجهزة الكمبيوتر المحمولة</div>
+        <div className="lg:text-2xl opacity-75 md:mt-8 mt-4">
           اكتشف أفضل أجهزة الكمبيوتر المحمولة في دمشق
         </div>
       </div>
