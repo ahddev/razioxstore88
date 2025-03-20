663b277ecea890eaa4e6719c9dbfa65be803620f
Ahdwka <ahed.wka4g@gmail.com>
2025-03-20 07:28:01 +0300

    hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 import "@fontsource/ibm-plex-sans-arabic/400.css"; // Regular weight
 import "@fontsource/ibm-plex-sans-arabic/700.css"; // Bold weight
-import BioCard from "@/components/ui/bio-cards";
-import { LaptopMinimal, Search, Star } from "lucide-react";
-import { BlurFade } from "@/components/magicui/blur-fade";
 import { Hero } from "@/components/Hero";
 
 export default function Home() {
