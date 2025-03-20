242f825f487daaaa469b0851e602badbe397df3e
Ahdwka <ahed.wka4g@gmail.com>
2025-03-20 07:26:35 +0300

    hero

--- app/page.tsx
+++ app/page.tsx
@@ -10,29 +10,7 @@ export default function Home() {
     <div className="container text-center text-primary px-2 mx-auto">
       <Hero />
 
-      <div className="md:mt-10 md:flex gap-10 text-center">
-        <BlurFade delay={0.25 * 5} inView>
-          <BioCard
-            icon={LaptopMinimal}
-            heading={"تشكيلة واسعة"}
-            description={"مجموعة متنوعة من أفضل العلامات التجارية العالمية"}
-          />
-        </BlurFade>
-        <BlurFade delay={0.25 * 6} inView>
-          <BioCard
-            icon={Search}
-            heading={"بحث متقدم"}
-            description={"ابحث حسب المواصفات والسعر والعلامة التجارية"}
-          />
-        </BlurFade>
-        <BlurFade delay={0.25 * 7} inView>
-          <BioCard
-            icon={Star}
-            heading={"خدمة متميزة"}
-            description={"دعم فني متخصص ومساعدة في اختيار الجهاز المناسب"}
-          />
-        </BlurFade>
-      </div>
+      
     </div>
   );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,6 +7,8 @@ import Link from "next/link";
 import { BlurFade } from "./magicui/blur-fade";
 import { BorderBeam } from "./magicui/border-beam";
 import { Particles } from "./magicui/particles";
+import BioCard from "./ui/bio-cards";
+import { LaptopMinimal, Search, Star } from "lucide-react";
 
 
 export function Hero() {
@@ -59,7 +61,29 @@ export function Hero() {
           </div>{" "}
 
         </BlurFade>
-
+        <div className="md:mt-10 md:flex gap-10 text-center">
+        <BlurFade delay={0.25 * 5} inView>
+          <BioCard
+            icon={LaptopMinimal}
+            heading={"تشكيلة واسعة"}
+            description={"مجموعة متنوعة من أفضل العلامات التجارية العالمية"}
+          />
+        </BlurFade>
+        <BlurFade delay={0.25 * 6} inView>
+          <BioCard
+            icon={Search}
+            heading={"بحث متقدم"}
+            description={"ابحث حسب المواصفات والسعر والعلامة التجارية"}
+          />
+        </BlurFade>
+        <BlurFade delay={0.25 * 7} inView>
+          <BioCard
+            icon={Star}
+            heading={"خدمة متميزة"}
+            description={"دعم فني متخصص ومساعدة في اختيار الجهاز المناسب"}
+          />
+        </BlurFade>
+      </div>
       </span>
       <Particles
         className="absolute inset-0 z-0"
